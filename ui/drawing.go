1916fcc7e85765479d65666896cd39ca4aa37d58
Martin Zimandl <martin.zimandl@gmail.com>
2021-07-23 15:04:48 +0200

    fonts file

--- ui/drawing.go
+++ ui/drawing.go
@@ -1,6 +1,10 @@
 package ui2d
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"strconv"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
 
 func (ui *ui) drawBox(rect *sdl.Rect, color sdl.Color) {
 	ui.whiteDot.SetColorMod(color.R, color.G, color.B)
@@ -10,4 +14,29 @@ func (ui *ui) drawBox(rect *sdl.Rect, color sdl.Color) {
 	ui.renderer.Copy(ui.whiteDot, nil, &sdl.Rect{rect.X, rect.Y, rect.W, 1})
 	ui.renderer.Copy(ui.whiteDot, nil, &sdl.Rect{rect.X, rect.Y + rect.H - 1, rect.W, 1})
 	ui.renderer.Copy(ui.whiteDot, nil, &sdl.Rect{rect.X + rect.W - 1, rect.Y, 1, rect.H})
+}
+
+func (ui *ui) stringToTexture(s string, fontType FontType) *sdl.Texture {
+	font, exists := ui.fonts[fontType]
+	if exists {
+		textKey := TextCacheKey{fontType, s}
+		texture, exists := ui.textCache[textKey]
+		if exists {
+			return texture
+		}
+
+		fontSurface, err := font.RenderUTF8Blended(s, sdl.Color{255, 255, 255, 0})
+		if err != nil {
+			panic(err)
+		}
+		fontTexture, err := ui.renderer.CreateTextureFromSurface(fontSurface)
+		if err != nil {
+			panic(err)
+		}
+
+		ui.textCache[textKey] = fontTexture
+		return fontTexture
+	} else {
+		panic("Font type not found: " + strconv.Itoa(int(fontType)))
+	}
 }
--- ui/fonts.go
+++ ui/fonts.go
@@ -1,9 +1,6 @@
 package ui2d
 
 import (
-	"strconv"
-
-	"github.com/veandco/go-sdl2/sdl"
 	"github.com/veandco/go-sdl2/ttf"
 )
 
@@ -34,30 +31,5 @@ func (ui *ui) loadFonts() {
 	ui.fonts[FontLarge], err = ttf.OpenFont("ui/assets/Kingthings_Foundation.ttf", int(64*float64(ui.winWidth)/1280))
 	if err != nil {
 		panic(err)
-	}
-}
-
-func (ui *ui) stringToTexture(s string, fontType FontType) *sdl.Texture {
-	font, exists := ui.fonts[fontType]
-	if exists {
-		textKey := TextCacheKey{fontType, s}
-		texture, exists := ui.textCache[textKey]
-		if exists {
-			return texture
-		}
-
-		fontSurface, err := font.RenderUTF8Blended(s, sdl.Color{255, 255, 255, 0})
-		if err != nil {
-			panic(err)
-		}
-		fontTexture, err := ui.renderer.CreateTextureFromSurface(fontSurface)
-		if err != nil {
-			panic(err)
-		}
-
-		ui.textCache[textKey] = fontTexture
-		return fontTexture
-	} else {
-		panic("Font type not found: " + strconv.Itoa(int(fontType)))
 	}
 }
